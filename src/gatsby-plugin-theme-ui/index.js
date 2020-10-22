6ae4be37b968da35957f45f7cc68ad31a552f054
Shawn Lauzon <shawn.lauzon@gmail.com>
2020-10-22 17:15:15 +0700

    Set colors

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,13 @@
+import novelaTheme from "@narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui";
+
+export default {
+  ...novelaTheme,
+  colors: {
+    ...novelaTheme.colors,
+    primary: "#9ADEDC",
+    secondary: "#BBE7E3",
+    accent: "#E7331A",
+    grey: "#73737D",
+    background: "#EFF7E5",
+  },
+};
