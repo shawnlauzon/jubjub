d41ca2299c677077e07b7bcdef8fa53eb4ebc991
Shawn Lauzon <shawn.lauzon@gmail.com>
2020-10-23 09:29:25 +0700

    Show proper icons for social media

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,16 +14,16 @@ module.exports = {
     // important to set the main text that appears in the hero
     hero: {
       heading: `Jackie's guide to Koh Phangan`,
-      maxWidth: 752,
+      maxWidth: 52,
     },
     // Used for social links in the root footer
     social: [
       {
-        name: `Twitter`,
+        name: "twitter",
         url: `https://twitter.com/jubjub`,
       },
       {
-        name: `GitHub`,
+        name: "github",
         url: `https://github.com/jubjub`,
       },
     ],
